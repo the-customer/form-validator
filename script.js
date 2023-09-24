080c36446188a60bc771c7ee5248dcc0f91f8aba
the-customer <niangalytall@gmail.com>
2023-09-24 10:37:22 +0000

    Form-valid

---
+++ script.js
@@ -0,0 +1,82 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+//------------Functions
+function showError(input,message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+//
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+//
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(re.test(input.value.trim().toLowerCase())){
+        showSuccess(input);
+    }else{
+        showError(input,'Email is not valid!');
+    }
+}
+//
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input){
+        if(input.value === ''){
+            showError(input,`${getFieldName(input)} is required!`)
+        }else{
+            showSuccess(input);
+        }
+    });
+}
+//
+function checkLength(input, min,max){
+    if(input.value.length < min){
+        showError(input,`${getFieldName(input)} must be at least ${min} characters!`);
+    }else if(input.value.length > max){
+        showError(input,`${getFieldName(input)} must be less than ${max} characters!`);
+    }else{
+        showSuccess(input);
+    }
+}
+//
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+//
+function checkPasswordMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,'Password are not match!');
+    }
+}
+
+//------------Events
+
+
+
+form.addEventListener('submit',function (e) {
+    e.preventDefault(); 
+    checkRequired([username,email,password,password2]);
+    checkLength(username,3,15);
+    checkLength(password,6,25);
+    checkEmail(email);
+    checkPasswordMatch(password,password2);
+});
+
+
+  function onSelectedPhoto() {
+    const photo = document.getElementById('image');
+    var f = new FileReader();
+    f.readAsDataURL(photo.files[0]);
+    f.onloadend = function (event) {
+      const path = event.target.result;
+      const img = document.querySelector('#photo').setAttribute('src', path);
+    }
+}
